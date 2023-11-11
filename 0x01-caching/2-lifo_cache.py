1eedd40f91cae4a6cac8e78b47280f1d9c55bea5
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-11-11 03:53:10 +0000

    done

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -11,14 +11,15 @@ class LIFOCache(BaseCaching):
         super().__init__()
 
     def put(self, key, item):
-        """doc"""
+        """documentation"""
         if key is not None and item is not None:
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS \
                     and key not in self.cache_data.keys():
-                        last_key, last_value = self.cache_data.popitem()
-                        print("DISCARD: {}". format(last_key))
+                last_key, last_value = self.cache_data.popitem()
+                print("DISCARD: {}". format(last_key))
+
             self.cache_data[key] = item
 
     def get(self, key):
-        """doc"""
+        """docs"""
         return self.cache_data.get(key)
