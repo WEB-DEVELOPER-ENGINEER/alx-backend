5551c867657c243568df49e82bf7af6406058333
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-11-11 06:49:12 +0000

    done

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+MRU Caching
+"""
+from collections import OrderedDict
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """
+    class MRUCache
+    """
+    def __init__(self):
+        super().__init__()
+        self.mru_order = OrderedDict()
+
+    def put(self, key, item):
+        """
+        Assign to the dict 'self.cache_data' the value for the key 'key'
+        """
+        if not key or not item:
+            return
+        self.mru_order[key] = self.cache_data[key] = item
+        if len(self.cache_data) > self.MAX_ITEMS:
+            item_discarded = next(iter(self.mru_order))
+            del self.cache_data[item_discarded]
+            print("DISCARD:", item_discarded)
+            self.mru_order.popitem(last=False)
+        self.mru_order.move_to_end(key, False)
+
+    def get(self, key):
+        """
+        returns the value in self.cache_data linked to key
+        """
+        if key in self.cache_data:
+            self.mru_order.move_to_end(key, False)
+            return self.cache_data[key]
+        return None
