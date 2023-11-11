9dd26c917609e0cec8be1edd1ef6506614a73b17
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-11-11 06:31:47 +0000

    done

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,33 +1,42 @@
 #!/usr/bin/env python3
-"""Create a class LRUCache"""
+"""
+LRU Caching
+"""
+from collections import OrderedDict
 BaseCaching = __import__('base_caching').BaseCaching
 
 
 class LRUCache(BaseCaching):
-    """class"""
+    """
+    class LRUCache
+    """
 
     def __init__(self):
-        """intialize"""
+        """
+        Init method
+        """
         super().__init__()
-        self.usedKeys = []
+        self.lru_order = OrderedDict()
 
     def put(self, key, item):
-        """doc"""
-        if key is not None and item is not None:
+        """
+        assign to the dict 'self.cache_data' the item value for the key 'key'
+        """
+        if key and item:
+            self.lru_order[key] = item
+            self.lru_order.move_to_end(key)
             self.cache_data[key] = item
-            if key not in self.usedKeys:
-                self.usedKeys.append(key)
-            else:
-                self.usedKeys.append(
-                    self.usedKeys.pop(self.usedKeys.index(key)))
-            if len(self.usedKeys) > BaseCaching.MAX_ITEMS:
-                discard = self.usedKeys.pop(0)
-                del self.cache_data[discard]
-                print('DISCARD: {:s}'.format(discard))
+        if len(self.cache_data) > self.MAX_ITEMS:
+            item_discarded = next(iter(self.lru_order))
+            del self.cache_data[item_discarded]
+            print("DISCARD:", item_discarded)
+            self.lru_order.popitem(last=False)
 
     def get(self, key):
-        """doc"""
-        if key is not None and key in self.cache_data.keys():
-            self.usedKeys.append(self.usedKeys.pop(self.usedKeys.index(key)))
-            return self.cache_data.get(key)
+        """
+        Must return the value in self.cache_data linked to key.
+        """
+        if key in self.cache_data:
+            self.lru_order.move_to_end(key)
+            return self.cache_data[key]
         return None
