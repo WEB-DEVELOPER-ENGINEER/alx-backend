a22558c129e791f4c8560a748a8d109e52746948
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-11-11 03:36:13 +0000

    done

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""LIFO Caching"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """a caching system"""
+
+    def __init__(self):
+        """initialize"""
+        super().__init__()
+
+    def put(self, key, item):
+        """doc"""
+        if key is not None and item is not None:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS \
+                    and key not in self.cache_data.keys():
+                        last_key, last_value = self.cache_data.popitem()
+                print("DISCARD: {}". format(last_key))
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """doc"""
+        return self.cache_data.get(key)
