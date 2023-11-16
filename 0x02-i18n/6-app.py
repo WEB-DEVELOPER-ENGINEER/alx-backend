03b7dffb87a146594d433d52881aa0c869dd98db
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-11-16 22:17:55 +0000

    done

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -64,7 +64,7 @@ def get_locale():
 @app.route('/')
 def index():
     """App"""
-    return render_template('6-index.html')
+    return render_template('5-index.html')
 
 
 if __name__ == '__main__':
